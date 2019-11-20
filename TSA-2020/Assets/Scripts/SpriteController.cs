babf9ca4a36c1b461a8f04ccc77074228012ae52
Will B <electromanx11@gmail.com>
2019-11-19 21:35:56 -0800

    Created Sprite Controller
    Including turn and movement functionality. Made in a way so that it will probably be useless within a few days.

---
+++ TSA-2020/Assets/Scripts/SpriteController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+
+    public float movementSpeed = 0;
+    public float rotationSpeed = 0;
+    public bool useInputAxis = false;
+    public string axisName = "";
+    public LayerMask ground;
+
+
+    public bool rightFace = true;
+    private SpriteRenderer mySpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake() 
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = 0;
+        if(useInputAxis) {
+            translation = Input.GetAxis(axisName) * movementSpeed * Time.deltaTime;
+            transform.Translate(translation,0,0);
+        }
+
+        TurnIf(translation);
+        if(!rightFace) mySpriteRenderer.flipX = true;
+        else mySpriteRenderer.flipX = false; 
+    }
+
+    public void Turn() {
+        if(rightFace == true) rightFace = false;
+        else rightFace = true;
+    }
+
+    public void TurnIf(float translation) {
+        if(translation < 0 && rightFace) rightFace = false;
+        if(translation > 0 && !rightFace) rightFace = true;
+    }
+}
